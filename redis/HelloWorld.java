beb5d7b22c3ccda3f699e6dac5c5e1e78bac93b7
ggstudy11 <439403849@qq.com>
2024-12-20 14:50:03 +0800

    类加载器原理

---
+++ redis/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    static final String a1 = "abc";
+    static final String a2 = "abc";
+    static final String abc = "abc";
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
